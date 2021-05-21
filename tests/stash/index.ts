6e885d9515ec595525b44a5509b8898bc201bf20
basyoukajiki <okaki.cs@gmail.com>
2021-05-21 22:39:13 +0900

    stashの作成

---
+++ tests/stash/index.ts
@@ -0,0 +1,14 @@
+// 一時避難
+
+// package.json
+/*
+"gitHooks": {
+    "pre-commit": "lint-staged"
+  },
+  "lint-staged": {
+    "*.{js,jsx,vue,ts,tsx}": [
+      "vue-cli-service lint",
+      "git add"
+    ]
+  }
+ */
